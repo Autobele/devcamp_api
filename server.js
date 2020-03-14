9dff8b153def688eb3b74817041286f5ffe719e4
Autobele Silva <autobele@msn.com>
2020-03-13 21:10:23 -0300

    add connection mongodb and configure unhandleRejection

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Routes files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectionDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env'});
 
+// Connect to database
+connectionDB();
+
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps/', bootcamps);
@@ -15,4 +25,11 @@ app.use('/api/v1/bootcamps/', bootcamps);
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV;
 
-app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+})
